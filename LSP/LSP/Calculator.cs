ee167959f52dded00d3505c38723a8ce2af00c0a
Marinko <marinkospasojevic@gmail.com>
2021-02-21 14:03:49 +0100

    Added missing class

---
+++ LSP/LSP/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP
+{
+	public abstract class Calculator
+	{
+        protected readonly int[] _numbers;
+
+        public Calculator(int[] numbers)
+        {
+            _numbers = numbers;
+        }
+        public abstract int Calculate();
+    }
+}
